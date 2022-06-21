ee049ca3c0da98b52aaba3d2b45d17a3ceccafc0
Dmitriy Mamontov <mamontovdmitriy@mail.ru>
2022-06-21 15:29:42 +0300

    throw exception

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Exception\InvalidArgumentException;
 
 #[Route(path: '/api')]
 class ApiController extends AbstractController
@@ -21,6 +22,10 @@ class ApiController extends AbstractController
     public function restoreAddress(Request $request, WalletService $walletService): Response
     {
         $privateKey = $request->get('private_key');
+        if ($privateKey === null) {
+            throw new InvalidArgumentException('The param "private_key" is required.');
+        }
+
         $address = $walletService->restoreAddress($privateKey)->getRawData();
 
         return $this->json($address);
